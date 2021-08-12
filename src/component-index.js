8f234bbacd67a6ef19980f89350575b85c669e9d
Volodymyr Basok <volodymyrquo@gmail.com>
2021-08-12 11:15:25 +0300

    excluded component-index from gitignore

---
+++ src/component-index.js
@@ -0,0 +1,51 @@
+/*
+Copyright 2018 New Vector Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+ * THIS FILE IS AUTO-GENERATED
+ * You can edit it you like, but your changes will be overwritten,
+ * so you'd just be trying to swim upstream like a salmon.
+ * You are not a salmon.
+ */
+
+let components = {};
+import structures$VectorEmbeddedPage from './components/structures/VectorEmbeddedPage';
+structures$VectorEmbeddedPage && (components['structures.VectorEmbeddedPage'] = structures$VectorEmbeddedPage);
+import structures$auth$SumraLogin from './components/structures/auth/SumraLogin';
+structures$auth$SumraLogin && (components['structures.auth.SumraLogin'] = structures$auth$SumraLogin);
+import views$auth$SumraPasswordLogin from './components/views/auth/SumraPasswordLogin';
+views$auth$SumraPasswordLogin && (components['views.auth.SumraPasswordLogin'] = views$auth$SumraPasswordLogin);
+import views$elements$SSOButtons from './components/views/elements/SSOButtons';
+views$elements$SSOButtons && (components['views.elements.SSOButtons'] = views$elements$SSOButtons);
+import structures$auth$SumraConfirmCodeForm from './components/structures/auth/SumraConfirmCodeForm';
+structures$auth$SumraConfirmCodeForm && (components['structures.auth.SumraConfirmCodeForm'] = structures$auth$SumraConfirmCodeForm);
+import views$auth$SumraAuthBody from './components/views/auth/SumraAuthBody';
+views$auth$SumraAuthBody && (components['views.auth.SumraAuthBody'] = views$auth$SumraAuthBody);
+import views$auth$SumraAuthPage from './components/views/auth/SumraAuthPage';
+views$auth$SumraAuthPage && (components['views.auth.SumraAuthPage'] = views$auth$SumraAuthPage);
+import views$auth$SumraUserCreateForm from './components/views/auth/SumraUserCreateForm';
+views$auth$SumraUserCreateForm && (components['views.auth.SumraUserCreateForm'] = views$auth$SumraUserCreateForm);
+import views$auth$SumraWelcome from './components/views/auth/SumraWelcome';
+views$auth$SumraWelcome && (components['views.auth.SumraWelcome'] = views$auth$SumraWelcome);
+import views$auth$VectorAuthFooter from './components/views/auth/VectorAuthFooter';
+views$auth$VectorAuthFooter && (components['views.auth.VectorAuthFooter'] = views$auth$VectorAuthFooter);
+import views$auth$VectorAuthHeaderLogo from './components/views/auth/VectorAuthHeaderLogo';
+views$auth$VectorAuthHeaderLogo && (components['views.auth.VectorAuthHeaderLogo'] = views$auth$VectorAuthHeaderLogo);
+import views$auth$indexedDB from './components/views/auth/indexedDB';
+views$auth$indexedDB && (components['views.auth.indexedDB'] = views$auth$indexedDB);
+import views$auth$socialLinks from './components/views/auth/socialLinks';
+views$auth$socialLinks && (components['views.auth.socialLinks'] = views$auth$socialLinks);
+export {components};
